fb4e983d7a45da9d8edebfe8348791d844e0e398
TakingTheL <yliu6@stuy.edu>
2018-02-14 23:34:14 -0500

    works, but last function isn't what I wish it was

--- mango.py
+++ mango.py
@@ -1,3 +1,10 @@
+"""
+Yuanchu Liu and Tiffany Moi
+SoftDev2 pd7
+K04 -- Mi only nyam ital food, mon!
+2018-02-15
+"""
+
 from pymongo import MongoClient
 c = MongoClient("lisa.stuy.edu", 27017)
 
@@ -26,4 +33,26 @@ def get_zip_score(zip_code = "11368", score = 10):
     for each in cursor:
         print each
 
-get_zip_score()
+def restaurant_search(name = "null", zip_code = "null", cuisine = "null"):
+    """
+    TODO
+    Expand to a full search, make parameters optional
+        Challenges:
+            eval("col.find({ <param_in_string_form_here> })") doesn't recognize col as MongoClient("lisa.stuy.edu",27017).test.restaurants (does not keep track of vars)
+                (also eval() is bad)
+            The closest thing to a wildcard query is <param>:{$exist = true}, but that isn't a string and so cannot be easily replaced in col.find() by a string
+        Result:
+            Sad function here with unnecessary default args and filtering
+    """
+    if name != "null":
+        name_search_param = name
+    if zip_code != "null":
+        zip_search_param = zip_code
+    if cuisine != "null":
+        cuisine_search_param = cuisine
+
+    cursor = col.find({"name":name_search_param, "address.zipcode":zip_search_param, "cuisine":cuisine_search_param})
+    for each in cursor:
+        print each
+
+restaurant_search("Jing Way Food","11220","Other")
