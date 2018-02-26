282f53258c4e99d84b44d0a1ce4141695202bb40
TakingTheL <yliu6@stuy.edu>
2018-02-26 12:45:08 -0500

    some frontend stuff

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import urllib2, json
+from utils import db
 
 app = Flask(__name__)
-
 
 #base 
 @app.route("/")
 def start():
-    return 
+    return render_template("home.html")
+
+@app.route("/results")
+def results():
     
+    return render_template("results.html")
 
 
 if __name__ == "__main__":
+    
     app.debug = True
     app.run()
