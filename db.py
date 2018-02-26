6006f8169602a92d30817ec22b3fdc9d04d7c324
tmoi29 <tmoi@stuy.edu>
2018-02-25 20:58:01 -0500

    done

--- db.py
+++ db.py
@@ -1,8 +1,8 @@
 """
 Yuanchu Liu and Tiffany Moi
 SoftDev2 pd7
-K04 -- Mi only nyam ital food, mon!
-2018-02-15
+K05 -- Import/Export Bank
+2018-02-26
 """
 
 from pymongo import MongoClient
@@ -11,6 +11,7 @@ import urllib2, json
 
 """
 SETUP
+
 
 def load_json():
     url = "http://api.nobelprize.org/v1/laureate.json"
@@ -25,11 +26,13 @@ db = c["mango"]
 json = load_json()
 
 for laureate in json["laureates"]:
-    db.laureates.insert(laureate)"""
+    db.laureates.insert(laureate)
+    """
     
 c = MongoClient("lisa.stuy.edu", 27017)  
 db = c.mango
 col = db.laureates
+
 
 #Finds Laureates born in a given country
 def search_birthCountry(country = "USA"):
@@ -45,16 +48,27 @@ def search_deathCountry(country = "USA"):
         print each
 #search_deathCountry()
 
+#Finds Laureates from a given country who won a prize in a given category
 def search_birth_category(country = "USA", cat = "physics"):
     cursor = col.find({"diedCountry":country, "prizes.category": cat})
     for each in cursor:
         print each
-search_birth_category()
+#search_birth_category()
 
-
-#Finds Laureates who recieved award under a given age
-
-def search_age(age = 60):
-    cursor = col.find({"borough":borough})
+#Finds Laureates that won a prize in a given year
+def search_year_won(year = 2000):
+    cursor = col.find({"prizes.year": str(year)})
     for each in cursor:
         print each
+#search_year_won()
+
+#Finds Laureates with a given name
+
+def search_name(name = "Albert Einstein"):
+    surname = name.split(" ")[1]
+    first = name.split(' ')[0]
+    cursor = col.find({"$and":[{"surname": surname}, {"firstname": first}]})
+    for each in cursor:
+        print each
+search_name()
+
